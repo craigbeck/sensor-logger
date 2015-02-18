2c1775a2d86150acb017dc7aac1c866f22edf40d
Craig Beck <luckybonza@gmail.com>
2015-02-17 18:48:35 -0800

    automatically grab serial address of first Arduino found

--- listen.js
+++ listen.js
@@ -1,6 +1,7 @@
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 var util = require("util");
+var _ = require("lodash");
 
 var baudrate = process.argv[2] || 57600;
 var parser = serialport.parsers.readline("\n");
@@ -10,38 +11,54 @@ var librato = require("librato-metrics").createClient({
   token: process.env.LIBRATO_KEY
 });
 
-var port = new SerialPort("/dev/tty.usbmodemfa131", {
-  baudrate: 57600,
-  parser: parser
-});
-
 var log = function () {
   var message = util.format.apply(util.format, arguments);
   console.log("%s - %s", new Date(), message);
 };
 
-port.on("open", function () {
-  log("open!");
-  port.on("data", function (data) {
-    try {
-      var json = JSON.parse(data);
-      log("DATA OK", JSON.stringify(json));
-      librato.post("/metrics", {
-        gauges: [
-          { name: "tempC", value: json.tempC, source: "desk" },
-          { name: "pressurePa", value: json.pressurePa, source: "desk" },
-          { name: "altitude", value: json.realAtltitudeM, source: "desk" }
-        ]
-      }, function (err, response) {
-        if (err) {
-          log("ERR", err);
-        }
-      });
-    } catch (e) {
-      log("DATAERR", data.toString());
-    }
-  });
-});
+serialport.list(function (err, comPorts) {
+  if (err) {
+    return console.error("Could not list com ports", err);
+  }
 
-log("starting...");
+  var portdef = _(comPorts)
+    .find(function (comPort) {
+      return comPort.vendorId === "0x2341";
+    });
+
+  if (!portdef) {
+    return console.error("No Ardnuino found");
+  }
+  log("Found Arduino on", portdef.comName);
+
+  var port = new SerialPort(portdef.comName, {
+    baudrate: 57600,
+    parser: parser
+  });
+
+  port.on("open", function () {
+    log("open!");
+    port.on("data", function (data) {
+      try {
+        var json = JSON.parse(data);
+        log("DATA OK", JSON.stringify(json));
+        librato.post("/metrics", {
+          gauges: [
+            { name: "tempC", value: json.tempC, source: "desk" },
+            { name: "pressurePa", value: json.pressurePa, source: "desk" },
+            { name: "altitude", value: json.realAtltitudeM, source: "desk" }
+          ]
+        }, function (err, response) {
+          if (err) {
+            log("ERR", err);
+          }
+        });
+      } catch (e) {
+        log("DATAERR", data.toString());
+      }
+    });
+  });
+
+  log("starting...");
+});
 
